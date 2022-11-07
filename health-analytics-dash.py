842582d93831b2473560b94c94a2e45200ee52ce
Higgins <joshiggins@deloitte.com>
2022-11-07 17:58:22 -0500

    added data

--- health-analytics-dash.py
+++ health-analytics-dash.py
@@ -121,7 +121,7 @@ with tab3:
                                 size="Total Population",
                                 color_continuous_scale=px.colors.cyclical.IceFire, 
                                 size_max=15, 
-                                zoom=5)
+                                zoom=5).show()
 
         cw1.plotly_chart(fig, use_container_width=True) 
         
