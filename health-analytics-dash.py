f2dbd88adea113b1b68a0b93015d0bfa3e245257
Higgins <joshiggins@deloitte.com>
2022-11-07 17:56:53 -0500

    added data

--- health-analytics-dash.py
+++ health-analytics-dash.py
@@ -110,9 +110,9 @@ with tab3:
         g2.plotly_chart(fig, use_container_width=True)  
 
         cw1 = st.columns((4))
-        
+
         # Choropleth
-        test_df = pd.read_excel(sheet_name='geo_data')
+        test_df = pd.read_excel('health-analytics-data.xlsx', sheet_name='geo_data')
         px.set_mapbox_access_token("pk.eyJ1Ijoiam9laGlnZ2kxNzU4IiwiYSI6ImNsOWZ1NGkzZDJubnIzeGw5NHAxcjZyeDQifQ.OxUPPPmwro-Vm_58P1B3UQ")
         fig = px.scatter_mapbox(test_df, 
                                 lat="Latitude", 
