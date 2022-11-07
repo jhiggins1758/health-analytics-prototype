a7511f50bab90c547c458e565187869b72a30994
Higgins <joshiggins@deloitte.com>
2022-11-07 12:37:51 -0500

    added data

--- health-analytics-dash.py
+++ health-analytics-dash.py
@@ -45,7 +45,7 @@ with st.spinner('Updating Report...'):
     m4.metric(label = 'Time Lost today (Above 15 mins)',value = str(int(hl['Value']))+" Hours", delta = str(int(hl['Previous']))+' Compared to yesterday')
     m1.write('')
      
-    # Number of Completed Handovers by Hour
+    # Target Population
     g1, g2, g3 = st.columns((1,1,1))
     
     fgdf = pd.read_excel('DataforMock.xlsx',sheet_name = 'Graph')
@@ -84,20 +84,6 @@ with st.spinner('Updating Report...'):
                       template='seaborn')
     
     g1.plotly_chart(plot, use_container_width=True)
-    
-    # Predicted Number of Arrivals
-    
-    fcst = pd.read_excel('DataforMock.xlsx',sheet_name = 'Forecast')
-    
-    fcst = fcst[fcst['Hospital Attended']==hosp]
-    
-    fig = px.bar(fcst, x = 'Arrived Destination Resolved', y='y', template = 'seaborn')
-    
-    fig.update_traces(marker_color='#7A9E9F')
-    
-    fig.update_layout(title_text="Predicted Number of Arrivals",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
-    
-    g2.plotly_chart(fig, use_container_width=True)  
     
     # Average Completed Handover Duration by hour
 
